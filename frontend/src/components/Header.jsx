d20b41ce7b0a6ca7f69a834d6ac760b10247280d
babuprasadravi <babuprasad784@gmail.com>
2024-03-25 11:58:10 +0530

    Add cart item count badge to the cart icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,14 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap'
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png'
 
 function Header() {
+
+    const {cartItems} = useSelector(state => state.cart);
+    // console.log(cartItems)
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -17,8 +22,21 @@ function Header() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className='ms-auto'>
-                          <LinkContainer to='/cart'><Nav.Link><FaShoppingCart />Cart</Nav.Link></LinkContainer>  
-                          <LinkContainer to='/login'><Nav.Link><FaUser />Sign In</Nav.Link></LinkContainer>
+                          <LinkContainer to='/cart'>
+                            <Nav.Link>
+                                <FaShoppingCart />Cart
+                                {cartItems && cartItems.length > 0 && (
+                                <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                    {cartItems.reduce((acc, item) => acc + Number(item.qty), 0)}
+                                </Badge>
+                               )}
+                            </Nav.Link>
+                        </LinkContainer>  
+                          <LinkContainer to='/login'>
+                            <Nav.Link>
+                                <FaUser />Sign In
+                            </Nav.Link>
+                        </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
